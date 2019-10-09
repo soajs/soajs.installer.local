cf78353a0816d8c9afac9fbdb981e48aef48e938
antoinehage <me@antoinehage.com>
2019-10-09 18:59:53 -0400

    using soajs logger

--- libexec/utils/utils.js
+++ libexec/utils/utils.js
@@ -1,23 +1,9 @@
 "use strict";
 
-//const bunyan = require("bunyan");
-//const bunyanFormat = require('bunyan-format');
 const soajs = require('soajs');
 
 module.exports = {
 	"getLogger": () => {
 		return soajs.core.getLogger("SOAJS Installer", {"formatter": {"levelInString": true, "outputMode": 'short'}, "level": "debug"});
-		/*
-		//set the logger
-		let formatOut = bunyanFormat({"levelInString": true, "outputMode": 'short'});
-		const logger = bunyan.createLogger({
-			name: "SOAJS Installer",
-			"src": true,
-			"level": "debug",
-			"stream": (process.env.SOAJS_CONSOLE) ? null : formatOut
-		});
-		
-		return logger;
-		*/
 	}
 };
