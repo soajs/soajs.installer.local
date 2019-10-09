9e659ed6260c6b2e49670b0056ece8e125dcdf02
antoinehage <me@antoinehage.com>
2019-10-09 18:21:55 -0400

    first commit

---
+++ libexec/utils/utils.js
@@ -0,0 +1,23 @@
+"use strict";
+
+//const bunyan = require("bunyan");
+//const bunyanFormat = require('bunyan-format');
+const soajs = require('soajs');
+
+module.exports = {
+	"getLogger": () => {
+		return soajs.core.getLogger("SOAJS Installer", {"formatter": {"levelInString": true, "outputMode": 'short'}});
+		/*
+		//set the logger
+		let formatOut = bunyanFormat({"levelInString": true, "outputMode": 'short'});
+		const logger = bunyan.createLogger({
+			name: "SOAJS Installer",
+			"src": true,
+			"level": "debug",
+			"stream": (process.env.SOAJS_CONSOLE) ? null : formatOut
+		});
+		
+		return logger;
+		*/
+	}
+};
